77d5b7d7b45caf016a4a50f59c27f84c571465cc
felfelita <felitadiyanti@gmail.com>
2024-11-06 14:34:26 +0700

    done tutorial 7

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,99 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:mental_health_tracker/widgets/left_drawer.dart';
+import 'package:mental_health_tracker/moodentry_form.dart';
+
 class MyHomePage extends StatelessWidget {
-    MyHomePage({super.key});
-    final String npm = '2306207165'; // NPM
-    final String name = 'peli'; // Nama
-    final String className = 'PBP C'; // Kelas
-    final List<ItemHomepage> items = [
-         ItemHomepage("Lihat Mood", Icons.mood),
-         ItemHomepage("Tambah Mood", Icons.add),
-         ItemHomepage("Logout", Icons.logout),
-    ];
-    @override
-    Widget build(BuildContext context) {
-      // Scaffold menyediakan struktur dasar halaman dengan AppBar dan body.
-      return Scaffold(
-        // AppBar adalah bagian atas halaman yang menampilkan judul.
-        appBar: AppBar(
-          // Judul aplikasi "Mental Health Tracker" dengan teks putih dan tebal.
-          title: const Text(
-            'Mental Health Tracker',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
+  MyHomePage({super.key});
+  final String npm = '2306207165'; // NPM
+  final String name = 'Felita Zahra Diyanti'; // Nama
+  final String className = 'PBP C'; // Kelas
+
+  final List<ItemHomepage> items = [
+    ItemHomepage("Lihat Mood", Icons.mood),
+    ItemHomepage("Tambah Mood", Icons.add),
+    ItemHomepage("Logout", Icons.logout),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold menyediakan struktur dasar halaman dengan AppBar dan body.
+    return Scaffold(
+      // AppBar adalah bagian atas halaman yang menampilkan judul.
+      appBar: AppBar(
+        // Judul aplikasi "Mental Health Tracker" dengan teks putih dan tebal.
+        title: const Text(
+          'Mental Health Tracker',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
           ),
-          // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
-          backgroundColor: Theme.of(context).colorScheme.primary,
         ),
-        // Body halaman dengan padding di sekelilingnya.
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          // Menyusun widget secara vertikal dalam sebuah kolom.
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // Row untuk menampilkan 3 InfoCard secara horizontal.
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      drawer: const LeftDrawer(),
+      // Body halaman dengan padding di sekelilingnya.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        // Menyusun widget secara vertikal dalam sebuah kolom.
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Row untuk menampilkan 3 InfoCard secara horizontal.
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InfoCard(title: 'NPM', content: npm),
+                InfoCard(title: 'Name', content: name),
+                InfoCard(title: 'Class', content: className),
+              ],
+            ),
+
+            // Memberikan jarak vertikal 16 unit.
+            const SizedBox(height: 16.0),
+
+            // Menempatkan widget berikutnya di tengah halaman.
+            Center(
+              child: Column(
+                // Menyusun teks dan grid item secara vertikal.
+
                 children: [
-                  InfoCard(title: 'NPM', content: npm),
-                  InfoCard(title: 'Name', content: name),
-                  InfoCard(title: 'Class', content: className),
-                ],
-              ),
-
-              // Memberikan jarak vertikal 16 unit.
-              const SizedBox(height: 16.0),
-
-              // Menempatkan widget berikutnya di tengah halaman.
-              Center(
-                child: Column(
-                  // Menyusun teks dan grid item secara vertikal.
-
-                  children: [
-                    // Menampilkan teks sambutan dengan gaya tebal dan ukuran 18.
-                    const Padding(
-                      padding: EdgeInsets.only(top: 16.0),
-                      child: Text(
-                        'Welcome to Mental Health Tracker',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
-                        ),
+                  // Menampilkan teks sambutan dengan gaya tebal dan ukuran 18.
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16.0),
+                    child: Text(
+                      'Welcome to Mental Health Tracker',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
                       ),
                     ),
+                  ),
 
-                    // Grid untuk menampilkan ItemCard dalam bentuk grid 3 kolom.
-                    GridView.count(
-                      primary: true,
-                      padding: const EdgeInsets.all(20),
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 10,
-                      crossAxisCount: 3,
-                      // Agar grid menyesuaikan tinggi kontennya.
-                      shrinkWrap: true,
+                  // Grid untuk menampilkan ItemCard dalam bentuk grid 3 kolom.
+                  GridView.count(
+                    primary: true,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 3,
+                    // Agar grid menyesuaikan tinggi kontennya.
+                    shrinkWrap: true,
 
-                      // Menampilkan ItemCard untuk setiap item dalam list items.
-                      children: items.map((ItemHomepage item) {
-                        return ItemCard(item);
-                      }).toList(),
-                    ),
-                  ],
-                ),
+                    // Menampilkan ItemCard untuk setiap item dalam list items.
+                    children: items.map((ItemHomepage item) {
+                      return ItemCard(item);
+                    }).toList(),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
-    }
+      ),
+    );
   }
+}
 
 class InfoCard extends StatelessWidget {
   // Kartu informasi yang menampilkan title dan content.
 
-  final String title;  // Judul kartu.
-  final String content;  // Isi kartu.
+  final String title; // Judul kartu.
+  final String content; // Isi kartu.
 
   const InfoCard({super.key, required this.title, required this.content});
 
@@ -104,7 +111,8 @@ class InfoCard extends StatelessWidget {
       elevation: 2.0,
       child: Container(
         // Mengatur ukuran dan jarak di dalam kartu.
-        width: MediaQuery.of(context).size.width / 3.5, // menyesuaikan dengan lebar device yang digunakan.
+        width: MediaQuery.of(context).size.width /
+            3.5, // menyesuaikan dengan lebar device yang digunakan.
         padding: const EdgeInsets.all(16.0),
         // Menyusun title dan content secara vertikal.
         child: Column(
@@ -123,18 +131,18 @@ class InfoCard extends StatelessWidget {
 }
 
 class ItemHomepage {
-     final String name;
-     final IconData icon;
+  final String name;
+  final IconData icon;
 
-     ItemHomepage(this.name, this.icon);
- }
+  ItemHomepage(this.name, this.icon);
+}
 
 class ItemCard extends StatelessWidget {
   // Menampilkan kartu dengan ikon dan nama.
 
-  final ItemHomepage item; 
-  
-  const ItemCard(this.item, {super.key}); 
+  final ItemHomepage item;
+
+  const ItemCard(this.item, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -143,16 +151,22 @@ class ItemCard extends StatelessWidget {
       color: Theme.of(context).colorScheme.secondary,
       // Membuat sudut kartu melengkung.
       borderRadius: BorderRadius.circular(12),
-      
+
       child: InkWell(
         // Aksi ketika kartu ditekan.
         onTap: () {
           // Menampilkan pesan SnackBar saat kartu ditekan.
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
-            ..showSnackBar(
-              SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!"))
-            );
+            ..showSnackBar(SnackBar(
+                content: Text("Kamu telah menekan tombol ${item.name}!")));
+
+          if (item.name == "Tambah Mood") {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const MoodEntryFormPage()));
+          }
         },
         // Container untuk menyimpan Icon dan Text
         child: Container(
@@ -180,6 +194,4 @@ class ItemCard extends StatelessWidget {
       ),
     );
   }
-  
 }
-
